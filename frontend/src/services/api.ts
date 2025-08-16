0b4653444a1ae44791fe578f87108cea73bda63a
NeilMisra99 <116110498+NeilMisra99@users.noreply.github.com>
2025-08-16 04:08:53 -0400

    fix: small update to redeploy frontend

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -21,7 +21,7 @@ const api = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
-  timeout: 10000, // 10 second timeout
+  timeout: 10000,
 });
 
 export const feedbackAPI = {
