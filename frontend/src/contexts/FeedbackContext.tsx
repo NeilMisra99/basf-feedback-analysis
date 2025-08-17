0b15d51e86e17931093755ec8b268a5372fac218
NeilMisra99 <116110498+NeilMisra99@users.noreply.github.com>
2025-08-16 21:52:54 -0400

    chore: cleanup

--- frontend/src/contexts/FeedbackContext.tsx
+++ frontend/src/contexts/FeedbackContext.tsx
@@ -59,6 +59,7 @@ export function FeedbackProvider({ children }: { children: ReactNode }) {
   );
 }
 
+// eslint-disable-next-line react-refresh/only-export-components
 export function useFeedback() {
   const context = useContext(FeedbackContext);
   if (context === undefined) {
